0ca2a9b598fc4a2952968d0e2d57d86cb5a10a06
GyabaahFelix <felixic360@gmail.com>
2025-05-09 22:56:22 +0000

    Done with day1. Lezz progress later

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,11 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="landing" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/SignUp" options={{ headerTransparent: true,
+        headerTitle: ''
+       }}
+       />
+      <Stack.Screen name="(auth)/SignIn" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/landing.tsx
+++ app/landing.tsx
@@ -1,9 +1,11 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import React from "react";
 import Colors from "@/data/Colors";
 import Button from "@/components/Shared/Button";
+import { useRouter } from "expo-router";
 
 export default function LandingScreen() {
+  const router=useRouter();
   return (
     <View>
       <Image
@@ -31,14 +33,16 @@ export default function LandingScreen() {
           Connect, share, and explore campus life — all in one place.
         </Text>
 
-       <Button text="Get Started" onPress={() => console.log('Button pressed')} />
+       <Button text="Get Started" onPress={() => router.push('/(auth)/SignUp')} />
 
+          <Pressable onPress={() => router.push('/(auth)/SignIn')}>
         <Text style={{
             fontSize: 16,
             textAlign: "center",
             color: Colors.GRAY,
             marginTop: 7,
         }}>Already have an account? Sign In Here</Text>
+        </Pressable>
       </View>
     </View>
   );
