c2e6f63f57b3da194e6d337a443ce7a1f189c06d
GyabaahFelix <felixic360@gmail.com>
2025-05-13 01:41:04 +0000

    Continued designing SignIn page

--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import Colors from "@/data/Colors";
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={{ paddingTop: 60, padding: 20 }}>
+      <Text
+        style={{
+          fontSize: 25,
+          fontWeight: "bold",
+        }}
+      >
+        Create New Account
+      </Text>
+
+      <View style={{ display: "flex", alignItems: "center" }}>
+        <View>
+          <Image
+            source={require("../../assets/images/profile.png")}
+            style={{ width: 100, height: 100, borderRadius: 99, marginTop: 20 }}
+          />
+          <Entypo
+            name="camera"
+            size={24}
+            color={Colors.PRIMARY}
+            style={{ position: "absolute", bottom: 0, right: 0 }}
+          />
+        </View>
+      </View>
     </View>
-  )
+  );
 }
